8d5abfd9dbf4908151323e35aa8dedbbd9ee13f6
jtm <james@mathews.top>
2020-09-25 22:58:08 +0300

    driveby cleanups
    cleaning up 8 year old cold

--- pywolf3d/cube.py
+++ pywolf3d/cube.py
@@ -40,29 +40,27 @@ def load_textures():
     global flat_types
     global default_texture
     global door_types
-    
+
     wall_types,flat_types,door_types = load_walls()
-    
+
 
 class Cube(object):
-    
-    
+
     def __init__(self, position, wall_type,rendered_normals=[0,1,2,3]):
-        
         self.position = position
         self.wall_type_code = wall_type
         self.rendered_normals = rendered_normals
-        
+
         if(wall_types == None):
             load_textures()
-            
+
         self.wall_type = wall_types[self.wall_type_code]
-    
+
     num_faces = 5
     top_face = 4
     dark_faces = [0,1]
     light_faces = [2,3]
-        
+
     vertices = [ (0.0, 0.0, 1.0),
                  (1.0, 0.0, 1.0),
                  (1.0, 1.0, 1.0),
@@ -71,25 +69,25 @@ class Cube(object):
                  (1.0, 0.0, 0.0),
                  (1.0, 1.0, 0.0),
                  (0.0, 1.0, 0.0) ]
-    
+
     t_index = [  (0.0, 0.0),
                  (1.0, 0.0),
                  (1.0, 1.0),
                  (0.0, 1.0), ]
-        
+
     normals = [ (0.0, 0.0, +1.0),  # front
                 (0.0, 0.0, -1.0),  # back
                 (+1.0, 0.0, 0.0),  # right
-                (-1.0, 0.0, 0.0),  # left 
+                (-1.0, 0.0, 0.0),  # left
                 (0.0, +1.0, 0.0),  # top
                 (0.0, -1.0, 0.0) ] # bottom
-    
+
     vertex_indices = [ (0, 1, 2, 3),  # front
                        (4, 5, 6, 7),  # back
                        (1, 5, 6, 2),  # right
                        (0, 4, 7, 3),  # left
                        (3, 2, 6, 7),  # top
-                       (0, 1, 5, 4) ] # bottom    
+                       (0, 1, 5, 4) ] # bottom
 
 
     def render(self):
@@ -97,22 +95,22 @@ class Cube(object):
             return self.render_texture()
         else:
             return self.render_color()
-        
-        
+
+
     def render_texture(self):
         #Load dark texture
         bind_texture(self.wall_type.texture1)
         vertices = [tuple(Vector3(v) + self.position) for v in self.vertices]
-        
+
         glBegin(GL_QUADS)
-    
+
         #Draw the 4 textured sides
         for face_no in self.light_faces:
             if(face_no != self.top_face and face_no in self.rendered_normals):
                 glNormal3dv( self.normals[face_no] )
-                
+
                 v1, v2, v3, v4 = self.vertex_indices[face_no]
-    
+
                 glTexCoord2fv(self.t_index[0])
                 glVertex( vertices[v1] )
                 glTexCoord2fv(self.t_index[1])
@@ -120,20 +118,20 @@ class Cube(object):
                 glTexCoord2fv(self.t_index[2])
                 glVertex( vertices[v3] )
                 glTexCoord2fv(self.t_index[3])
-                glVertex( vertices[v4] )            
+                glVertex( vertices[v4] )
 
         glEnd()
-        
+
         #Now the dark faces
         bind_texture(self.wall_type.texture2)
-        
+
         glBegin(GL_QUADS)
         for face_no in self.dark_faces:
             if(face_no != self.top_face and face_no in self.rendered_normals):
                 glNormal3dv( self.normals[face_no] )
-                
+
                 v1, v2, v3, v4 = self.vertex_indices[face_no]
-            
+
                 glTexCoord2fv(self.t_index[0])
                 glVertex( vertices[v1] )
                 glTexCoord2fv(self.t_index[1])
@@ -141,66 +139,53 @@ class Cube(object):
                 glTexCoord2fv(self.t_index[2])
                 glVertex( vertices[v3] )
                 glTexCoord2fv(self.t_index[3])
-                glVertex( vertices[v4] ) 
+                glVertex( vertices[v4] )
         glEnd()
-        
 
-        #if(self.top_face in self.rendered_normals):
-        #    glColor( black )
-        #    glBegin(GL_QUADS)
-        #    glNormal3dv( self.normals[self.top_face] )
-        #    v1, v2, v3, v4 = self.vertex_indices[self.top_face]
-        #    glVertex( vertices[v1] )
-        #    glVertex( vertices[v2] )
-        #    glVertex( vertices[v3] )
-        #    glVertex( vertices[v4] ) 
-        #    glEnd()
-        
 
-    def render_color(self):                
-        
+    def render_color(self):
         gl_col = colour_maps.get(self.wall_type,default_colour)
         glEnable(GL_COLOR_MATERIAL)
         glColor( gl_col )
-    
+
         # Adjust all the vertices so that the cube is at self.position
         vertices = [tuple(Vector3(v) + self.position) for v in self.vertices]
-            
+
         #Now the dark sdes
         glBegin(GL_QUADS)
-    
+
         for face_no in self.light_faces:
             if(face_no != self.top_face and face_no in self.rendered_normals):
                 glNormal3dv( self.normals[face_no] )
-                
+
                 v1, v2, v3, v4 = self.vertex_indices[face_no]
-                        
+
                 glVertex( vertices[v1] )
                 glVertex( vertices[v2] )
                 glVertex( vertices[v3] )
-                glVertex( vertices[v4] )            
-            
+                glVertex( vertices[v4] )
+
         glEnd()
-        
+
         #now the light side
         #gl_col = darken_colour(colour_maps.get(self.wall_type,default_colour))
         glColor( gl_col )
-        
+
         glBegin(GL_QUADS)
         for face_no in self.dark_faces:
             if(face_no != self.top_face and face_no in self.rendered_normals):
                 glNormal3dv( self.normals[face_no] )
-                
+
                 v1, v2, v3, v4 = self.vertex_indices[face_no]
-                        
+
                 glVertex( vertices[v1] )
                 glVertex( vertices[v2] )
                 glVertex( vertices[v3] )
-                glVertex( vertices[v4] )            
-            
+                glVertex( vertices[v4] )
+
         glEnd()
-        
-        
+
+
         if(self.top_face in self.rendered_normals):
             glColor( black )
             glBegin(GL_QUADS)
@@ -209,11 +194,11 @@ class Cube(object):
             glVertex( vertices[v1] )
             glVertex( vertices[v2] )
             glVertex( vertices[v3] )
-            glVertex( vertices[v4] ) 
+            glVertex( vertices[v4] )
             glEnd()
-        
-        
-        
+
+
+
 
 class Door(Cube):
     '''A door, that you can open'''
@@ -234,7 +219,7 @@ class Door(Cube):
 
         if(not door_types):
             load_textures()
-        
+
         #print door_types
         self.door_type = door_types[door_type]
 
@@ -246,18 +231,18 @@ class Door(Cube):
             self.side_faces = [2,3]
             self.door_faces = [0,1]
             self.door_offset = [0,0,0.5]
-    
-    
-    def render_texture(self):       
+
+
+    def render_texture(self):
         bind_texture(self.door_type.side_texture)
         vertices = [tuple(Vector3(v) + self.position) for v in self.vertices]
 
         glBegin(GL_QUADS)
         for face_no in self.side_faces:
             glNormal3dv( self.normals[face_no] )
-                
+
             v1, v2, v3, v4 = self.vertex_indices[face_no]
-            
+
             glTexCoord2fv(self.t_index[0])
             glVertex( vertices[v1] )
             glTexCoord2fv(self.t_index[1])
@@ -265,7 +250,7 @@ class Door(Cube):
             glTexCoord2fv(self.t_index[2])
             glVertex( vertices[v3] )
             glTexCoord2fv(self.t_index[3])
-            glVertex( vertices[v4] ) 
+            glVertex( vertices[v4] )
         glEnd()
 
         #Now render the door
@@ -283,22 +268,11 @@ class Door(Cube):
         glTexCoord2fv(self.t_index[2])
         glVertex( vertices[v3] )
         glTexCoord2fv(self.t_index[3])
-        glVertex( vertices[v4] ) 
+        glVertex( vertices[v4] )
         glEnd()
 
-        
 
-        #if(self.top_face in self.rendered_normals):
-        #    glColor( black )
-        #    glBegin(GL_QUADS)
-        #    glNormal3dv( self.normals[self.top_face] )
-        #    v1, v2, v3, v4 = self.vertex_indices[self.top_face]
-        #    glVertex( vertices[v1] )
-        #    glVertex( vertices[v2] )
-        #    glVertex( vertices[v3] )
-        #    glVertex( vertices[v4] ) 
-        #    glEnd()
-        
+
 
 
 class FlatSurface(object):
@@ -320,13 +294,13 @@ class FlatSurface(object):
         self.h = h
         self.x = x
         self.y = y
-        
+
         self.vertices = ((x,self.height,y),(x+w,self.height,y),(x+w,self.height,y+h),(x,self.height,y+h))
         self.t_index = [  (0.0, 0.0),
                  (w, 0.0),
                  (w, h),
                  (0.0, h), ]
-    
+
     def render(self):
 #         self.render_color()
         if(rendering_opts['textures']):
@@ -334,15 +308,15 @@ class FlatSurface(object):
             #return self.render_color()
         else:
             return self.render_color()
-        
-        
 
-        
-        
+
+
+
+
     def render_texture(self):
         bind_texture(self.flat_type.texture)
         glBegin(GL_QUADS)
-        
+
         glNormal3dv( (0.0, +1.0, 0.0) )
         v1, v2, v3, v4 = self.vertices
         glTexCoord2fv(self.t_index[0])
@@ -352,24 +326,24 @@ class FlatSurface(object):
         glTexCoord2fv(self.t_index[2])
         glVertex( v3 )
         glTexCoord2fv(self.t_index[3])
-        glVertex( v4 ) 
+        glVertex( v4 )
         glEnd()
-    
+
     def render_color(self):
-        
+
         gl_col = default_colour
         glColor( gl_col )
         glBegin(GL_QUADS)
-        
+
         glNormal3dv( (0.0, +1.0, 0.0) )
         v1, v2, v3, v4 = self.vertices
         glVertex( v1 )
         glVertex( v2 )
         glVertex( v3 )
-        glVertex( v4 )  
+        glVertex( v4 )
         glEnd()
-        
-        
+
+
 
 class Floor(FlatSurface):
     '''
--- pywolf3d/firstopengl.py
+++ pywolf3d/firstopengl.py
@@ -1,9 +1,7 @@
 from game_options import rendering_opts
-from maze import generate_maze
-#from maze import generate_maze
 SCREEN_SIZE = (800, 600)
 
-from math import radians 
+from math import radians
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -20,39 +18,37 @@ from things import ImmovableThing, PassableThing, PickupThing
 
 
 def resize(width, height):
-    
+
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    
+
     if(rendering_opts['perspective']):
         gluPerspective(60.0, float(width)/height, .1, 1000.)
     else:
         glOrtho(-1,1,-1,1,-0.5,10);
-    
+
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
 
 def init():
-    
+
     glEnable(GL_DEPTH_TEST)
-    
+
     glShadeModel(GL_FLAT)
     glClearColor(1.0, 1.0, 1.0, 0.0)
 
     glEnable(GL_COLOR_MATERIAL)
-    
+
     #glEnable(GL_LIGHTING)
-    #glEnable(GL_LIGHT0)        
-    #glLight(GL_LIGHT0, GL_POSITION,  (0, 1, 1, 0))    
+    #glEnable(GL_LIGHT0)
+    #glLight(GL_LIGHT0, GL_POSITION,  (0, 1, 1, 0))
     glEnable (GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA)
-    
+
     #glBlendFunc (GL_ONE, GL_ONE)
 
-#test_map = [[1,1,1,1],[0,0,0,0],[0,0,0,0],[0,1,0,0],[1,1,1,1]]    
-#test_map = generate_maze(20,20)
 test_map  = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
              [1,0,0,0,1,0,0,3,3,3,3,3,3,3,3,3,3,3],
              [1,0,0,0,0,0,0,0,11,0,0,0,0,0,0,0,0,3],
@@ -73,59 +69,61 @@ test_map  = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 FPS=rendering_opts['fps']
 
 def run():
-    
+
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE, HWSURFACE|OPENGL|DOUBLEBUF)
-    
+
     resize(*SCREEN_SIZE)
     init()
-    
-    clock = pygame.time.Clock()    
-    
-    glMaterial(GL_FRONT, GL_AMBIENT, (0.1, 0.1, 0.1, 1.0))    
+
+    clock = pygame.time.Clock()
+
+    glMaterial(GL_FRONT, GL_AMBIENT, (0.1, 0.1, 0.1, 1.0))
     glMaterial(GL_FRONT, GL_DIFFUSE, (1.0, 1.0, 1.0, 1.0))
-    
+
 
     # This object renders the 'map'
     #print test_map
     object_list = [ ImmovableThing(2,2,3),PassableThing(2,3,2),PickupThing(2,4,1),PickupThing(1,1,1)]
-    game_map = GameMap(test_map,object_list)        
+    game_map = GameMap(test_map,object_list)
     player = game_map.add_player(2, 3)
 
 
     while True:
-        
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
             if event.type == KEYUP and event.key == K_ESCAPE:
-                return                
-            
+                return
+
         # Clear the screen, and z-buffer
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
+
 
         if(FPS):
             time_passed = clock.tick(FPS)
         else:
             time_passed = clock.tick()
         time_passed_seconds = time_passed / 1000.
-        
+
         pressed = pygame.key.get_pressed()
         player.do_input(pressed)
         player.update_vectors(time_passed_seconds)
-        
+
         # Upload the inverse camera matrix to OpenGL
         glLoadMatrixd(player.camera_matrix.get_inverse().to_opengl())
-                
+
         # Light must be transformed as well
-        glLight(GL_LIGHT0, GL_POSITION,  (0, 1.5, 1, 0)) 
-                
+        glLight(GL_LIGHT0, GL_POSITION,  (0, 1.5, 1, 0))
+
         # Render the map
         game_map.render()
         game_map.objects_render(player)
-       
+
         # Show the screen
         pygame.display.flip()
 
-run()
+
+if __name__ == '__main__':
+    run()
