c746a584be5a980d9ae600f3243148622a69b184
jtm <james@mathews.top>
2020-09-25 22:54:34 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='pywolf3d',
+      version='1.0',
+      description='',
+      author='jammers',
+      author_email='jammers@gmail.com',
+      url='https://github.com/jammers-ach/pywolf3d',
+      packages=['pywolf3d',],
+      install_requires=['pygame', 'pyopengl'],
+      dependency_links=[]
+     )
