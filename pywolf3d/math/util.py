7da323cc0203e946e00cbf43f366a7368473cd6b
jtm <james@mathews.top>
2020-09-27 11:48:30 +0300

    cleanup: math.util

--- pywolf3d/math/util.py
+++ pywolf3d/math/util.py
@@ -1,24 +1,19 @@
-
 from math import pi
 from math import sqrt
 
 def format_number(n, accuracy=6):
     """Formats a number in a friendly manner (removes trailing zeros and unneccesary point."""
-    
     fs = "%."+str(accuracy)+"f"
     str_n = fs%float(n)
     if '.' in str_n:
         str_n = str_n.rstrip('0').rstrip('.')
     if str_n == "-0":
         str_n = "0"
-    #str_n = str_n.replace("-0", "0")
     return str_n
-    
 
 def lerp(a, b, i):
     """Linear enterpolate from a to b."""
     return a+(b-a)*i
-
 
 def linear_distance(a,b):
     """Linear distance from a to b"""
