186f8391edc688c21385c667617be9c007919352
jtm <james@mathews.top>
2021-01-18 23:22:36 +0200

    add doors

--- pywolf3d/level.py
+++ pywolf3d/level.py
@@ -57,26 +57,31 @@ cube = (
     (1,1,1), #7
 )
 quadverts = {
-    'w': [cube[x] for x in (6 ,2 ,0, 4, 6, 0)],
-    'e': [cube[x] for x in (1 ,3 ,7, 1, 7, 5)],
-    'n': [cube[x] for x in (5 ,7 ,6, 5, 6, 4)],
-    's': [cube[x] for x in (0 ,2 ,3, 0, 3, 1)],
+    'w': [cube[x] for x in (6 ,2 ,0, 4, 6, 0)], # 0 2 6 4
+    'e': [cube[x] for x in (3 ,7 ,5, 1, 3, 5)], # 1 3 7 5
+    'n': [cube[x] for x in (7 ,6 ,4, 5, 7, 4)], # 4 5 6 7
+    's': [cube[x] for x in (2, 3, 1, 0, 2, 1)], # 0 1 2 3
 }
 
 class LevelMesh():
+    def wall_file_name(self, val, northsouth=False):
+        '''returns the filename for a wall code'''
+        wall_code = (val-1)*2
+        if northsouth:
+            wall_code += 1
+        fname = f'wall{wall_code:04d}'
+        return fname
+
     def __init__(self, floor_pane, wall_codes, door_codes, floor_codes):
         lo = LevelOptimiser(floor_pane,
                             wall_codes,
                             floor_codes,
                             door_codes)
         level = lo.optimise()
+        door_side_txt = load_texture("wall0100", path="wolfdata/extracted/")
 
         self.quad = load_model('quad')
-        dungeon = Entity(model=Mesh(),
-                         collision=True,
-                         collider='mesh',
-                         )
-        model = dungeon.model
+        self.mesh_entities = {}
         faces = 0
 
         for z, row in enumerate(level):
@@ -84,15 +89,45 @@ class LevelMesh():
             assert h == lo.h, f"row {z} is wrong length {lo.h} {h}"
             for x, val in enumerate(row):
                 if val in wall_codes:
+                    if val not in self.mesh_entities:
+                        self.mesh_entities[val] = Entity(model=Mesh(),
+                                                         collision=True,
+                                                         collider='mesh',
+                                                         texture=load_texture(self.wall_file_name(val), path="wolfdata/extracted/")
+                                                         )
+                    model = self.mesh_entities[val].model
                     for face in lo.external_walls(z,x):
+                        texture_file = self.wall_file_name(val, northsouth=face=='n' or face =='s')
+                        txt = load_texture(texture_file, path="wolfdata/extracted/")
                         model.vertices.extend(self.vertices(face, x, 0, z)) # add quad vertices, but offset.
-                        model.colors.extend((color.random_color(),) * len(self.quad.vertices)) # add vertex colors.
                         faces +=1
 
-        print(f"made {faces} faces")
+                if val in door_codes:
+                    if val not in self.mesh_entities:
+                        self.mesh_entities[val] = Entity(model=Mesh(),
+                                                        collision=True,
+                                                        collider='mesh',
+                                                        texture=door_side_txt)
+                    model = self.mesh_entities[val].model
 
-        model.uvs = (self.quad.uvs) * (lo.w * lo.h)
-        model.generate() # call to create the mesh
+                    face = "ew" if val %2 else "ns"
+                    if face == "ew":
+                        model.vertices.extend(self.vertices("n", x-1, 0, z))
+                        model.vertices.extend(self.vertices("s", x+1, 0, z))
+                        faces +=2
+                    if face == "ns":
+                        model.vertices.extend(self.vertices("e", x, 0, z-1))
+                        model.vertices.extend(self.vertices("w", x, 0, z+1))
+                        faces +=2
+
+
+        for k,v in self.mesh_entities.items():
+            v.model.uvs = (self.quad.uvs) * (lo.w * lo.h)
+            v.model.generate() # call to create the mesh
+
+        meshes = len(self.mesh_entities.items())
+        print(f"made {meshes} wall meshes")
+        print(f"made {faces} faces")
 
 
     def vertices(self, facing, x, y, z):
@@ -108,34 +143,29 @@ class Door(Entity):
     def __init__(self,texture_file, parent=scene, position=(0,0,0),
                  facing='ns'):
         txt = load_texture(texture_file, path="wolfdata/extracted/")
-        sidetxt = load_texture("wall0100", path="wolfdata/extracted/")
         x,y,z = position
 
-        z_off = 0
-        x_off = 0
         z_rot = 0
+        self.door_x = x+0.5
+        self.door_y = y+0.5
+        self.door_z = z+0.5
 
         self.facing = facing
         assert facing in ['ns', 'ew']
         if facing == 'ns':
             z_rot = 90
-            z_off = 0.5
         elif facing == 'ew':
             z_rot = 180
-            x_off = -0.5
         else:
             raise Exception(f"direction {facing} is not valid for a door")
 
         super().__init__(
         )
 
-        self.door_x = x
-        self.door_y = y+0.5
-        self.door_z = z
 
         self.door1 = Entity(
             parent=self,
-            position = (x, y+0.5, z),
+            position = (self.door_x, self.door_y, self.door_z),
             model = Plane((1,1)),
             texture = txt,
             color = color.white,
@@ -146,7 +176,7 @@ class Door(Entity):
         )
         self.door2 = Entity(
             parent=self,
-            position = (x, y+0.5, z),
+            position = (self.door_x, self.door_y, self.door_z),
             model = Plane((1,1)),
             texture = txt,
             color = color.white,
@@ -156,29 +186,6 @@ class Door(Entity):
             rotation_z=z_rot - 180,
         )
 
-        self.door_side1 = Entity(
-            parent=self,
-            position = (x+x_off, y+0.5, z+z_off),
-            model = Plane((1,1)),
-            texture = sidetxt,
-            color = color.white,
-            collision=False,
-            collider='box',
-            rotation_x=-90,
-            rotation_z=z_rot - 90,
-        )
-
-        self.door_side2 = Entity(
-            parent=self,
-            position = (x-x_off, y+0.5, z-z_off),
-            model = Plane((1,1)),
-            texture = sidetxt,
-            color = color.white,
-            collision=False,
-            collider='box',
-            rotation_x=-90,
-            rotation_z=z_rot + 90,
-        )
 
     def open(self):
         dz = -1 if self.facing == 'ns' else 0
@@ -207,7 +214,7 @@ class LevelLoader():
              [33,107,107,107,107,107,33],
              [33,107,107,107,107,107,33],
              [33,33,33,33,33,33,33]]
-    start = (1,5,1)
+    start = (3,5,3)
     object_list = []
 
     # https://devinsmith.net/backups/xwolf/tiles.html
@@ -275,19 +282,19 @@ class LevelLoader():
                                  self.floor_lists)
 
 
-        # for z, row in enumerate(level):
-            # h = len(row)
-            # assert h == self.h, f"row {z} is wrong length {self.h} {h}"
-            # for x, val in enumerate(row):
+        for z, row in enumerate(self.level):
+            h = len(row)
+            assert h == self.h, f"row {z} is wrong length {self.h} {h}"
+            for x, val in enumerate(row):
                 # if val == -1:
                     # continue
 
                 # if val in self.floor_lists:
                     # continue
-                # if val in self.door_lists:
-                    # face = "ew" if val %2 else "ns"
-                    # Door(self.door_file_name(val), position=(x,0,z),
-                         # facing=face)
+                if val in self.door_lists:
+                    face = "ew" if val %2 else "ns"
+                    Door(self.door_file_name(val), position=(x,0,z),
+                         facing=face)
 
                 # if val in self.wall_lists:
                     # for face in lo.external_walls(z,x):
@@ -309,7 +316,7 @@ class LevelLoader():
             if code in range(23, 74+1):
                 txt = SolidSprite.texture_filename(code)
                 y, x = coord
-                self.sprites.append(SolidSprite(txt, position=(x,0,y)))
+                self.sprites.append(SolidSprite(txt, position=(x+0.5,0,y+0.5)))
                 total_objects += 1
 
         print(f"make {total_objects} objects")
